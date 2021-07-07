611bcacffabcf4699a4b74876d79bf48d1d52b6a
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-07-06 19:39:27 -0500

    Created useFetch.js to pull API data and set up Home.js to render school data.

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,41 @@
+import useFetch from './useFetch';
+import { useState, useEffect } from 'react';
+
 const Home = () => {
+    const { data, isLoading, error } = useFetch('https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&id=240444&api_key=297S7932ybwihdF333i2X9RqrCYSABid2X3YqwpF')
+
+    const [schoolData, setSchoolData] = useState(null); 
+    
+    useEffect(() => {
+        console.log(data)
+        if (data) {
+            setSchoolData(...data)
+        }
+    }, [data])
+    
     return (
-        <h1>Home page</h1>
+        <div className="wrapper">
+            {schoolData && <div className="main-div">
+                <h1>{schoolData.school.name}</h1>
+                {schoolData.school.alias && <h2>{schoolData.school.alias}</h2>}
+                <h3>{schoolData.school.school_url}</h3>
+                <h3>{schoolData.school.city}, {schoolData.school.state} {schoolData.school.zip}</h3>
+                <p>Total No. of Students: {schoolData.latest.student.size}</p>
+                <p>Program Percentage:</p>
+                {Object.keys(schoolData.latest.academics.program_percentage).map((item, i) => (
+                    schoolData.latest.academics.program_percentage[item] !== 0 && <p key={i}>{i + 1} {item}: {schoolData.latest.academics.program_percentage[item]}</p>
+                ))}
+                <p>Race Ethnicity:</p>
+                {Object.keys(schoolData.latest.student.demographics.race_ethnicity).map((item, i) => (
+                    schoolData.latest.student.demographics.race_ethnicity[item] && <p key={i}>{i + 1} {item}: {schoolData.latest.student.demographics.race_ethnicity[item]}</p>
+                ))}
+                <h3>***Graph of Choice Data***</h3>
+            </div>}
+            <button>Save as PDF</button>
+            <button>Save as JSON</button>
+            <button>Print Page</button>
+        </div>
+        
      );
 }
  
---
+++ src/useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+//Custom hook to fetch API data
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => {
+                if(!res.ok) {
+                    throw Error("Unable to fetch data")
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data.results)
+                setIsLoading(false)
+                setError(null)
+            })
+            .catch(err => {
+                setIsLoading(false)
+                setError(err.message)
+            })
+    }, [url]);
+
+    return { data, isLoading, error }
+}
+
+export default useFetch
